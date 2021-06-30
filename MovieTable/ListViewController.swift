4439ad884b9b185e829b347e475926042e2d9ddc
LeeGaram <star146913@gmail.com>
2021-06-30 17:13:15 +0900

    subtitle table view

--- MovieTable/ListViewController.swift
+++ MovieTable/ListViewController.swift
@@ -34,4 +34,25 @@ class ListViewController: UITableViewController{
     override func viewDidLoad() {
         
 }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 주어진 행에 맞는 데이터 소스를 읽어온다.
+        let row = self.list[indexPath.row]
+        // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = row.title
+        
+        // 추가사항 : 서브타이틀에 데이터 연결
+        cell.detailTextLabel?.text = row.description
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        NSLog("선택된 행은 \(indexPath.row)번째 행입니다.")
+    }
 }
