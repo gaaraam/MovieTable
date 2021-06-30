c019a686160f7a366ef6a92aa2e979f27332af85
LeeGaram <star146913@gmail.com>
2021-06-30 15:37:39 +0900

    refactoring ListViewController using tuple

--- MovieTable/ListViewController.swift
+++ MovieTable/ListViewController.swift
@@ -8,32 +8,30 @@
 import UIKit
 
 class ListViewController: UITableViewController{
+    
+    // 튜플 아이템으로 구성된 데이터 세트
+        var datest = [
+            ("센과 치히로의 행방불명", "치히로의 기이한 경험", "2001-00-00",9.00),
+            ("하울의 움직이는 성", "하울과 소피의 사랑", "2004-00-00", 8.00),
+            ("이웃집 토토로", "토토로는 괴물이었을까?", "1988-00-00", 8.00)
+        ]
+    
         // 테이블 뷰를 구성할 리스트 데이터
-    var list = [MovieVO]()
+    lazy var list : [MovieVO] = {
+        var datalist = [MovieVO]()
+        for(title, desc, opendate, rating) in self.datest{
+            let mvo = MovieVO()
+            mvo.title = title
+            mvo.description = desc
+            mvo.opendate = opendate
+            mvo.rating = rating
+            
+            datalist.append(mvo)
+        }
+        return datalist
+    }()
     
     override func viewDidLoad() {
-        var mvo = MovieVO( )
-        mvo.title = "센과 치히로의 행방불명"
-        mvo.description = "치히로의 기이한 경험"
-        mvo.opendate = "2001-07-00"
-        mvo.rating = 9.00
-        // 배열에 추가
-        self.list.append(mvo)
         
-        mvo = MovieVO( )
-        mvo.title = "하울의 움직이는 성"
-        mvo.description = "하울과 소피의 사랑이야기"
-        mvo.opendate = "2004-00-00"
-        mvo.rating = 8.00
-        // 배열에 추가
-        self.list.append(mvo)
-        
-        mvo = MovieVO( )
-        mvo.title = "이웃집 토토로"
-        mvo.description = "토토로는 괴물이었을까?"
-        mvo.opendate = "1988-00-00"
-        mvo.rating = 8.00
-        // 배열에 추가
-        self.list.append(mvo)
-    }
+}
 }
